72384e87a496ef65116fe412f51656e8c4b37453
tranminhan <trananminhan17@gmail.com>
2024-09-08 22:14:51 +0700

    Labwork 1+2+3+4+5+6

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -22,6 +22,10 @@ public class WeatherActivity extends AppCompatActivity {
         });
         ForecastFragment firstFragment = new ForecastFragment();
         getSupportFragmentManager().beginTransaction().add(R.id.main, firstFragment).commit();
+
+        WeatherFragment secondFragment = new WeatherFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.main, secondFragment).commit();
+
     }
 
     @Override
